9a468ff7a3ccd8ec6ca9989e230840652909ff72
Md. Abdullah Al-Mamoon <almamoon18@gmail.com>
2022-11-11 17:44:22 +0600

    getting all services done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +9,33 @@ const port = process.env.PORT || 5000;
 // middlewares
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dqmis3n.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const serviceCollection = client.db('dream-services').collection('services');
+
+        //getting all services from database
+        app.get('/services', async(req, res)=>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const allServices = await cursor.toArray();
+            res.send(allServices);
+        })
+    }
+    finally{}
+}
+
+run().catch(err=>{
+    console.log(err);
+})
+
+
 
 app.get('/', (req, res)=>{
     res.send("Dream Consultancy Server is running");
